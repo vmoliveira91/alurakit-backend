26a136fa555f2f92efe9194e87bb570b31fe1a31
Vitor Oliveira <vmoliveira91@gmail.com>
2021-07-20 22:54:27 -0300

    Criação do BD com Prisma Schema

---
+++ prisma/migrations/20210721014807_create_database/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Video" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "titulo" TEXT NOT NULL,
+    "descricao" TEXT NOT NULL,
+    "url" TEXT NOT NULL
+);
